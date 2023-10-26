40a2c4c144f028019ae285c68e3c41a48647c4ba
Lauren <lauren.m.lavelle@wmich.edu>
2023-10-25 20:36:13 -0400

    updated typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,42 +1,42 @@
 const typeDefs = `
-  type me {
-    
+  type Query {
+    me:User
   }
 
   type User {
     _id: ID
     username: String
-    email: 
-    bookCount: 
-    savedBooks
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
   }
 
-  type authors {
-    bookId: 
-    authors: 
-    description:
-    title:
-    image:
-    link:
+  type Book {
+    bookId: ID
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link:String
   }
   type Auth {
-    token:
-    user:
+    token: ID!
+    user: User
   }
-
+  input BookInput{
+    authors: [Strings]
+    description: String
+    title: String
+    bookId: String
+    image: String
+    link: String
+  }
   type Mutation {
-   
+   login(email:String, password:String):Auth
+   addUser(username:String, email:String, password:String):Auth
+   saveBook(criteria:BookInput):User
+   removeBook: (ID):User
   }
 `;
 
 module.exports = typeDefs;
-// Query type:me: Which returns a User type.
-// Mutation type:
-
-// login: Accepts an email and password as parameters; returns an Auth type.
-
-// addUser: Accepts a username, email, and password as parameters; returns an Auth type.
-
-// saveBook: Accepts a book author's array, description, title, bookId, image, and link as parameters; returns a User type. (Look into creating what's known as an input type to handle all of these parameters!)
-
-// removeBook: Accepts a book's bookId as a parameter; returns a User type.
