2fa95077b0d83d98fd5f89c336fef83d492641e4
Lauren <lauren.m.lavelle@wmich.edu>
2023-11-01 19:50:01 -0400

    cleaning code

--- client/src/pages/SavedBooks.jsx
+++ client/src/pages/SavedBooks.jsx
@@ -20,42 +20,12 @@ const SavedBooks = () => {
     QUERY_ME,
     'me'
   ]});
-  // const [userData, setUserData] = useState({});
   
   const { loading, data } = useQuery(QUERY_ME, {
     fetchPolicy: 'cache-and-network',
   });
   console.log("saved book data", data)
   const userData = data?.me || [];
-  console.log(loading)
-  // const profiles = data?.profiles || [];
-  // use this to determine if `useEffect()` hook needs to run again
-  // const userDataLength = Object.keys(userData).length;
-
-  // useEffect(() => {
-  //   const getUserData = async () => {
-  //     try {
-  //       const token = Auth.loggedIn() ? Auth.getToken() : null;
-
-  //       if (!token) {
-  //         return false;
-  //       }
-
-  //       const response = await getMe(token);
-
-  //       if (!response.ok) {
-  //         throw new Error('something went wrong!');
-  //       }
-
-  //       const user = await response.json();
-  //       setUserData(user);
-  //     } catch (err) {
-  //       console.error(err);
-  //     }
-  //   };
-
-  //   getUserData();
-  // }, [userDataLength]);
 
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
   const handleDeleteBook = async (bookId) => {
@@ -78,11 +48,6 @@ const SavedBooks = () => {
       console.error(err);
     }
   };
-
-  // if data isn't here yet, say so
-  // if (loading) {
-  //   return <h2>LOADING...</h2>;
-  // }
 
   return (
     <>
